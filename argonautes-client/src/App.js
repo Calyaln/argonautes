51a20356f2311346064763ed0f64fd9fddd4a28b
Calypso Asline <aslinecalypso@gmail.com>
2021-03-16 20:48:26 +0100

    frontend ok

--- argonautes-client/src/App.js
+++ argonautes-client/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import AddArgonautes from "./components/AddArgonautes";
 import ArgonautesList from "./components/ArgonautesList";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <AddArgonautes /> <br />
+      <Header />
+      <AddArgonautes />
       <ArgonautesList />
+      <Footer />
     </div>
   );
 }
--- argonautes-client/src/components/ArgonautesList.js
+++ argonautes-client/src/components/ArgonautesList.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
+import ApiHandler from "../api/apiHandler";
 
-class ArgonautesList extends Component {
+const api = new ApiHandler();
+
+class ArgonautesList extends React.Component {
+  state = {
+    argonautes: [],
+  };
+
+  // to make the api call
+  componentDidMount() {
+    api
+      .get("/api/argonautes")
+      .then((apiResponse) => {
+        console.log(apiResponse);
+        this.setState({
+          argonautes: apiResponse.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <>
         <h2>Membres de l'équipage</h2>
-        <p>Name</p>
+        <ul className="member-list">
+          {this.state.argonautes.map((argonaute) => {
+            return (
+              <li className="member-item" key={argonaute._id}>
+                {argonaute.name}
+              </li>
+            );
+          })}
+        </ul>
       </>
     );
   }
