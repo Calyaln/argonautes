914c730bc0de269065f2fa35ebee3270626be803
Calypso Asline <aslinecalypso@gmail.com>
2021-03-17 14:02:15 +0100

    ignore

--- argonautes-server/app.js
+++ argonautes-server/app.js
@@ -4,8 +4,16 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
+
+// define who is allowed to talk to the API
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,5 +23,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // prefixed
 app.use("/api/argonautes", require("./routes/argonautes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
 
 module.exports = app;
